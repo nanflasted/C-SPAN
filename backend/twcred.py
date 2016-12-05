d072b1dbdcdb19aafd2602c9f412da50297f087b
Yiqian Chen <nanflasted@users.noreply.github.com>
2016-12-05 00:32:09 -0500

    update twitter credential file format: twcred.py

---
+++ backend/twcred.py
@@ -0,0 +1,6 @@
+def auth():
+	consumer_key = YOUR_CONSUMER_KEY_HERE
+	consumer_secret = YOUR_CONSUMER_SECRET_HERE
+	access_token = YOUR_ACCESS_KEY_HERE
+	access_secret = YOUR_ACCESS_SECRET_HERE
+	return consumer_key,consumer_secret,access_token,access_secret
