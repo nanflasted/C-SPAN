855dcbc9d252994df92ccd8f20dd1ab10da5ceeb
WJLIDDY <wjl36@case.edu>
2016-12-06 22:50:02 -0500

    Basic profile info

--- frontend/app/controllers/legislator_page_controller.rb
+++ frontend/app/controllers/legislator_page_controller.rb
@@ -1,5 +1,13 @@
+require_relative "us_state"
 class LegislatorPageController < ApplicationController
   def view
-    @message = "Test! #{Legislator.first.name}"
+    @legislator = Legislator.find(params[:id])
+    # Get name
+    @name = @legislator.name
+    # Get description, i.e. "Senator from New York"
+    party = @legislator.party.capitalize + (@legislator.party.include?("Demo") ? "ic" : "")
+    @description = "#{party} #{@legislator.role.capitalize} from #{US_State.abbreviation_to_full(@legislator.state)}"
+    # CODING HORROR here but that's ok
+    @profile_pic="http://localhost:3000/l_img/#{@legislator.id}"
   end
 end
