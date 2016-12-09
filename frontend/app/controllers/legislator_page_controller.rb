2ce5a3075ae0bca073f9ea66c2f8eb353dc20b8f
WJLIDDY <wjl36@case.edu>
2016-12-08 19:02:05 -0500

    space to like posts

--- frontend/app/controllers/legislator_page_controller.rb
+++ frontend/app/controllers/legislator_page_controller.rb
@@ -2,6 +2,7 @@ require_relative "us_state"
 
 class LegislatorPageController < ApplicationController
   helper_method :get_img_link
+  helper_method :get_likes_subtext
 
   def view
     @legislator = Legislator.find(params[:id])
@@ -22,4 +23,19 @@ class LegislatorPageController < ApplicationController
   def get_img_link(id)
     return "http://localhost:3000/l_img/#{id}"
   end
+
+  def get_likes_subtext(c)
+    likers = Like.where(cid: c.id).map{ |l| Legislator.find(l.lid)}
+    # now we have id's of all the people who like the content
+    if likers.count == 0
+        return "No one likes this post" 
+    elsif likers.count == 1
+        return "#{likers[0].name} likes this post"
+    elsif likers.count == 2
+        return "#{likers[0].name} and #{likers[1].name} like this post"
+    else
+        return "#{likers[0].name}, #{likers[1].name}, and {likers.count-2} more like this post"
+    end
+  end
+    
 end
