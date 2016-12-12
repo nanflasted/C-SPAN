2bc6a1f1051940d0493bf2af0e5292d0b476ce46
WJLIDDY <wjl36@case.edu>
2016-12-11 19:04:06 -0500

    refactor, combine backend and frontend

--- backend/twcred.py
+++
@@ -1,6 +0,0 @@
-def auth():
-	consumer_key = "mCcyKjC8Aj8Sh4AWprIjg9Mua"
-	consumer_secret = "DMLkjkEAExRLvzEgGF2M5BSkjhQW0DwYxJDhJgP7TjFDfwJndq"
-	access_token = "446227835-abbsj35HuuOy2SoyTDcuO6mmJSSp2fkmsHH4EZvT"
-	access_secret = "uOxM2uaCw0o731hRNvbwgXhTXJMjuqfYlikhsun0xeWD4"
-	return consumer_key,consumer_secret,access_token,access_secret
